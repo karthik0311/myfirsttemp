68b9c8fffcbb346c96920f9d66a6da8352bdcc3d
karthik0311 <karthikeee0311@yahoo.in>
2017-10-04 21:43:51 +0530

    myfirstcommit

---
+++ githubIonic/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+import { OrganisationsPage } from '../pages/organisations/organisations';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { GithubUsersTsProvider } from '../providers/github-users-ts/github-users-ts';
+import { GithubUsers } from '../providers/github-users';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganisationsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganisationsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    GithubUsers
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GithubUsersTsProvider
+  ]
+})
+export class AppModule {}
